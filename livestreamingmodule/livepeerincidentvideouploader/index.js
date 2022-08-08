d5f4d5ad8aed14623206ad7306f7f27aa8469921
seetadev <dev@seeta.in>
2022-08-08 19:36:58 +0530

    livepeer road incident video nft uploader

---
+++ livestreamingmodule/livepeerincidentvideouploader/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const proxy = createProxyMiddleware({
+    target: 'https://livepeer.com',
+    changeOrigin: true,
+    headers: {
+        authorization:  `Bearer  ${process.env.LP_API_KEY}`
+    }
+});
+
+
+app.use('/js',express.static(path.join(__dirname, 'public/js')));
+
+
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+app.get('/api/asset/:id', proxy);
+app.get('/api/task/:id', proxy);
+app.post('/api/asset/request-upload', proxy);
+app.post('/api/asset/transcode', proxy);
+app.post('/api/asset/:id/export', proxy);
+
+
+app.listen(PORT);
---
+++ livestreamingmodule/livepeerincidentvideouploader/public/js/sdk.js
@@ -0,0 +1,56 @@
+const apiOpts = {};
+
+async function mintNft() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    let ethereum = window.ethereum;
+    let chainId = window.ethereum.chainId;
+
+
+    const signer = provider.getSigner();
+    console.log("Account:", await signer.getAddress());
+    console.log(ethereum)
+    console.log(chainId);
+
+    const minter = new videonft.minter.FullMinter(apiOpts, { ethereum, chainId });
+    const mintBtn = document.getElementById('mint-btn');
+
+    console.log(minter);
+    // let file = document.getElementById('file').files[0];
+    let file = window.livepeer.files[0];
+    // console.log('file object', document.getElementById('file'));
+    let title = document.getElementById("title").value || "My NFT";
+    console.log(title)
+    console.log(file)
+    console.log('create asset', minter.api.createAsset);
+    let progressBar = document.getElementById('progress-bar')
+    mintBtn.innerText = "Creating Mintable Video Asset..."
+
+    let asset = await minter.api.createAsset(title, file, (progress) => {
+        console.log('progress: ', progress)
+        progressBar.style.width = `${progress * 100 }%`;
+    });
+
+    mintBtn.innerText = 'Minting....'
+    console.log("asset", asset);
+    // // optional, optimizes the video for the NFT
+    asset = await minter.api.nftNormalize(asset);
+    console.log(asset)
+    const nftMetadata = {
+        description: 'My NFT description',
+        traits: { 'my-custom-trait': 'my-custom-value' }
+    };
+    console.log("metadata", nftMetadata)
+
+    const nftInfo = await minter.createNft({
+        name: title,
+        file,
+        nftMetadata
+    });
+
+    mintBtn.innerText = `Minted Video with ID ${nftInfo.tokenId}`;
+    console.log(`minted NFT on contract ${nftInfo.contractAddress} with ID ${nftInfo.tokenId}`);
+    return nftInfo;
+}
